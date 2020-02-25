bc25becb208ee811fdd4aa8113f6bce57f6ca48f
fujita-kait <hiro.fujita@he.kanagawa-it.ac.jp>
2020-02-25 14:51:07 +0900

    Initial Commit

---
+++ UI-01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI-01
+//
+//  Created by 藤田 on 2020/02/25.
+//  Copyright © 2020 KAIT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
